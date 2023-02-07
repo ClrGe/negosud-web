d0d6dfe93dbde7585a6eba4079c6a2b78769e168
ClG <cg@jmg-conseil.eu>
2023-02-07 17:49:06 +0100

    cleaned up structure + open Cart modal when clicking icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script>
 	import '../app.postcss';
-	import Header from './Header.svelte';
 	import './styles.css';
-	import {Envelope, QuestionMarkCircle} from "svelte-heros-v2";
-	import {page} from "$app/stores";
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <div class="app flex flex-col bg-center repeat absolute top-0 w-full h-screen bg-black">
@@ -13,28 +12,9 @@
 		<slot />
 	</main>
 	<div class="flex justify-center items-center">
-	<a target="_blank" href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000006688014" class="text-[#CAB089F9]">L'ABUS D'ALCOOL EST DANGEREUX POUR LA SANTÉ, À CONSOMMER AVEC MODÉRATION.</a>
-</div>
-	<footer class="flex flex-col  text-white">
-		<ul class="relative  bg-contain list-none flex justify-center ">
-			<li class=" p-3 o" aria-current={$page.url.pathname.startsWith('/about') ? 'page' : undefined}>
-				<a href="/about" class="font-black tracking-wider">À propos de Negosud</a>
-			</li>
-			<li class="p-3">
-				<a href="/contact" class="font-black  tracking-wider ">Nous contacter</a>
-			</li>
-			<li class="p-3">
-				<a href="" class="font-black  tracking-wider ">Mentions légales</a>
-			</li>
-			<li class="p-3">
-				<a href="" class="font-black  tracking-wider ">FAQ</a>
-			</li>
-			<li flex items-center justify-center>
-				<!--<img src="src/lib/images/bandeau_legal.jpg">-->
-			</li>
-		</ul>
-
-	</footer>
+		<a target="_blank" href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000006688014" class="text-[#CAB089F9]">L'ABUS D'ALCOOL EST DANGEREUX POUR LA SANTÉ, À CONSOMMER AVEC MODÉRATION.</a>
+	</div>
+	<Footer />
 </div>
 
 <style>
