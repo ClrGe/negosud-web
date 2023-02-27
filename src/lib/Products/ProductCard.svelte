0f24cf606c5cb0482c3256c8399d9937b20f2817
ClrGe <cg@jmg-conseil.eu>
2023-02-27 22:23:01 +0100

    add details

--- src/lib/Products/ProductCard.svelte
+++ src/lib/Products/ProductCard.svelte
@@ -71,34 +71,35 @@
         </Button>
     </div>
 </Card>
-<Modal autoclose={false} bind:open={isOpenModal} class="w-full !bg-black/90 !font-extrabold rounded-lg border-2 border-white">
-    <div class="p-4 text-white bg-red-900"><p class="!font-extrabold !px-32  text-center uppercase text-5xl">{item.fullName}</p></div>
+<Modal autoclose={false} bind:open={isOpenModal} class="w-full !bg-gray-200 !font-extrabold rounded-lg border-2 border-white">
+    <div class="p-4 text-red-900"><p class="!font-extrabold !px-32  text-center uppercase text-5xl">{item.fullName}</p></div>
     <div class="bg-white border border-2 rounded-lg">
     <div class="flex  justify-center items-center flex-rox py-6">
-        <div class="col h-[550px]  min-w-[160px] !w-[800px] mx-12">
+        <div class="col max-h-full  min-w-[160px] !w-[800px] mx-12">
             <img alt="pinard" width="300px" class="image h-full" src='{item.picture}'/>
         </div>
         <div class="pr-12 text-black text-lg">
             <p class="py-8 text-justify italic"> {item.description} <br /></p>
-            <p class="py-2">Producteur :  <a href="#" class="underline text-red-900 italic">{item.producer == null ? "Inconnu" : item.producer.name}</a> <br /></p>
-            <p class="py-2">Catégorie :  {item.wineType} <br /></p>
-            <p class="py-2">Volume :  {item.volume} cL <br /></p>
-            <p class="py-2">Alcool :  {item.alcoholPercentage} ° <br /></p>
-            <p class="py-2">Année :  {item.yearProduced}<br /></p>
+            <p class="py-2">Producteur :  <a href="#" class="underline text-red-900 italic">{item.producer == null ? "Vigneron" : item.producer.name}</a> <br /></p>
+            <p class="py-2">Vin {item.wineType} - Label {item.wineLabel == null ? "Bio" : item.wineLabel.label} - {item.yearProduced}<br /></p>
+            <p class="py-2">{item.region == null ? "Gascogne" : item.region.name} ;  {item.country == null ? "France" : item.country.name} <br /></p>
+            <p class="py-2">{item.volume} cL -  {item.alcoholPercentage} ° </p>
         </div>
         </div>
     </div>
-    <div class=" !w-full text-white p-4 pb-0 bg-red-900 flex justify-center  items-center">
+    <div class=" !w-full text-red-900 p-4 pb-0 flex justify-center  items-center">
         <h2 class="!font-extrabold text-5xl text-center">{item.customerPrice}€ </h2> <p class="!text-2xs"> TTC</p><br />
+        <p class="!text-2xs"> / bouteille</p>
     </div>
-    <div class="btn-group pb-10 px-10 flex align-center justify-center" role="group">
-        <Button class="!bg-red-900 hover:!bg-black shadow-lg m-1 !text-white font-extrabold"
-                on:click={addToCart}>
+    <div class="btn-group flex flex-col" role="group">
+        <Button class=" !text-white shadow-lg !bg-red-900" on:click={() => addToCart(1)}>
             <ShoppingCart/>
             Acheter
         </Button>
-        <Button class="hover:!bg-red-900 !bg-white shadow-lg m-1 text-red-900 hover:!text-white" on:click={closeModal}>
-            <ArrowUturnRight/> Fermer
+        <h2 class="text-center text-red-900 pt-4 font-sans font-extrabold">Lot de 6 pour {(substractPercentage(item.customerPrice, 5) *6)} € (<span class="text-lg">-5%</span>)</h2>
+        <Button class="!my-2 !text-red-900 !font-extrabold !bg-white shadow-lg " on:click={() => addToCart(6)}>
+            <Star/>
+            En profiter
         </Button>
     </div>
 </Modal>
